0cf68c71bf61288549994779cb4e297ca0dfa873
ehaseeb <66061407+ehaseeb@users.noreply.github.com>
2020-05-28 14:07:44 +0500

    admob api
    admob api

---
+++ AdmobApi (13).js
@@ -0,0 +1,75 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var r = getRandomInt(0,9);
+var myArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; 
+var Aand = myArray[Math.floor(Math.random() * myArray.length)];
+var Band = myArray[Math.floor(Math.random() * myArray.length)];
+var Cand = myArray[Math.floor(Math.random() * myArray.length)];
+var Dand = myArray[Math.floor(Math.random() * myArray.length)];
+var Eand = myArray[Math.floor(Math.random() * myArray.length)];
+var Fand = myArray[Math.floor(Math.random() * myArray.length)];
+var Gand = myArray[Math.floor(Math.random() * myArray.length)];
+var Hand = myArray[Math.floor(Math.random() * myArray.length)];
+var Iand = myArray[Math.floor(Math.random() * myArray.length)];
+var Jand = myArray[Math.floor(Math.random() * myArray.length)];
+var Kand = myArray[Math.floor(Math.random() * myArray.length)];
+var Land = myArray[Math.floor(Math.random() * myArray.length)];
+var Mand = myArray[Math.floor(Math.random() * myArray.length)];
+var Nand = myArray[Math.floor(Math.random() * myArray.length)];
+var Oand = myArray[Math.floor(Math.random() * myArray.length)];
+var Pand = myArray[Math.floor(Math.random() * myArray.length)];
+var Qand = myArray[Math.floor(Math.random() * myArray.length)];
+var Rand = myArray[Math.floor(Math.random() * myArray.length)];
+var Sand = myArray[Math.floor(Math.random() * myArray.length)];
+var Tand = myArray[Math.floor(Math.random() * myArray.length)];
+var Uand = myArray[Math.floor(Math.random() * myArray.length)];
+var Vand = myArray[Math.floor(Math.random() * myArray.length)];
+var Wand = myArray[Math.floor(Math.random() * myArray.length)];
+var Xand = myArray[Math.floor(Math.random() * myArray.length)];
+var Yand = myArray[Math.floor(Math.random() * myArray.length)];
+var Zand = Aand+Band+Cand+Dand+Eand+Fand+Gand+Hand+Iand+Jand+Kand+Land+Mand+r+Nand+Oand+Pand+Qand+Rand+Sand+Tand+Uand+Vand+Wand+Xand+Yand;
+var _0x6717=["/mads/gma?preqs=11&amp;u_sd=0.75&amp;seq_num=17&amp;u_w=300&amp;msid=","&amp;js=afma-sdk-a-v18.3.0 &amp;ms=","ydQxMJDUNwhx3zoQbvxxTnedSkPx1u2iZLqabmoif28jNRLm7xa_IjpMvKdopb2uST1aWv53iiWjfFhX6Pe33MYyihzdsi-d-niVtDJ31pOqtdxlNNwduyzAybHTUaE_UMUp57MhfIb_PX4iPhMP7-TvuubfEZyvMLw2iV40JFkfRQRnu1ogO0suW5WNYQY-8Ur-OrUSgh30JPlX7PSkqGcICUa0rFvmSNSvfOdhL6Sp8jqK4FuuwdjDkBTKWqv7l2-90VapUfBFQL07XXZiIO_y3OwCnnSSDpH4CZuooBY0FYbVKl0uHJYHZ0AA&amp;mv=80230011.com.android.vending&amp;bas_off=0&amp;format=interstitial_mb&amp;oar=0&amp;imbf=8009&amp;net=wi&amp;app_name=","&amp;hl=en&amp;gnt=8&amp;u_h=480&amp;carrier=40445&amp;bas_on=0&amp;ptime=323524&amp;u_audio=1&amp;u_so=p&amp;output=html&amp;region=mobile_app&amp;u_tz=330&amp;client_sdk=1&amp;ex=1&amp;client=","&amp;slotname=","&amp;askip=1&amp;gsb=wi&amp;caps=interactiveVideo_th_autoplay_mediation_sdkAdmobApiForAds_di&amp;eisu=","SXlb0lWFZ-uETXTw7rG4VREj_whsbSpxrqoTc6zklwxbuqzopsz4fQEIjWUlmuuLwtrPK39iul0hC_9pvr7exm90VoZr69H74VSIUT&amp;et=131&amp;jsv=56"];
+var _0x6716=["/mads/gma?preqs=11&amp;u_sd=0.75&amp;seq_num=17&amp;u_w=300&amp;msid=","&amp;js=afma-sdk-a-v18.3.0 &amp;ms=","ydQxMJDUNwhx3zoQbvxxTnedSkPx1u2iZLqabmoif28jNRLm7xa_IjpMvKdopb2uST1aWv53iiWjfFhX6Pe33MYyihzdsi-d-niVtDJ31pOqtdxlNNwduyzAybHTUaE_UMUp57MhfIb_PX4iPhMP7-TvuubfEZyvMLw2iV40JFkfRQRnu1ogO0suW5WNYQY-8Ur-OrUSgh30JPlX7PSkqGcICUa0rFvmSNSvfOdhL6Sp8jqK4FuuwdjDkBTKWqv7l2-90VapUfBFQL07XXZiIO_y3OwCnnSSDpH4CZuooBY0FYbVKl0uHJYHZ0AA&amp;mv=80230011.com.android.vending&amp;bas_off=0&amp;format=300x250_mb&amp;oar=0&amp;imbf=8009&amp;net=wi&amp;app_name=","&amp;hl=en&amp;gnt=8&amp;u_h=250&amp;carrier=40445&amp;bas_on=0&amp;ptime=323524&amp;u_audio=1&amp;u_so=p&amp;output=html&amp;region=mobile_app&amp;u_tz=330&amp;client_sdk=1&amp;ex=1&amp;client=","&amp;slotname=","&amp;askip=1&amp;gsb=wi&amp;caps=interactiveVideo_th_autoplay_mediation_sdkAdmobApiForAds_di&amp;eisu=","SXlb0lWFZ-uETXTw7rG4VREj_whsbSpxrqoTc6zklwxbuqzopsz4fQEIjWUlmuuLwtrPK39iul0hC_9pvr7exm90VoZr69H74VSIUT&amp;et=131&amp;jsv=56"];
+var _0x6715=["/mads/gma?preqs=11&amp;u_sd=0.75&amp;seq_num=17&amp;u_w=300&amp;msid=","&amp;js=afma-sdk-a-v18.3.0 &amp;ms=","ydQxMJDUNwhx3zoQbvxxTnedSkPx1u2iZLqabmoif28jNRLm7xa_IjpMvKdopb2uST1aWv53iiWjfFhX6Pe33MYyihzdsi-d-niVtDJ31pOqtdxlNNwduyzAybHTUaE_UMUp57MhfIb_PX4iPhMP7-TvuubfEZyvMLw2iV40JFkfRQRnu1ogO0suW5WNYQY-8Ur-OrUSgh30JPlX7PSkqGcICUa0rFvmSNSvfOdhL6Sp8jqK4FuuwdjDkBTKWqv7l2-90VapUfBFQL07XXZiIO_y3OwCnnSSDpH4CZuooBY0FYbVKl0uHJYHZ0AA&amp;mv=80230011.com.android.vending&amp;bas_off=0&amp;format=300x50_mb&amp;oar=0&amp;imbf=8009&amp;net=wi&amp;app_name=","&amp;hl=en&amp;gnt=8&amp;u_h=50&amp;carrier=40445&amp;bas_on=0&amp;ptime=323524&amp;u_audio=1&amp;u_so=p&amp;output=html&amp;region=mobile_app&amp;u_tz=330&amp;client_sdk=1&amp;ex=1&amp;client=","&amp;slotname=","&amp;askip=1&amp;gsb=wi&amp;caps=interactiveVideo_th_autoplay_mediation_sdkAdmobApiForAds_di&amp;eisu=","SXlb0lWFZ-uETXTw7rG4VREj_whsbSpxrqoTc6zklwxbuqzopsz4fQEIjWUlmuuLwtrPK39iul0hC_9pvr7exm90VoZr69H74VSIUT&amp;et=131&amp;jsv=56"];
+var _0x28f9=["\x3c\x69\x66\x72\x61\x6d\x65\x20\x73\x72\x63\x3d\x22","\x22\x20\x68\x65\x69\x67\x68\x74\x3d\x22\x34\x38\x30\x22\x20\x77\x69\x64\x74\x68\x3d\x22\x33\x32\x30\x22\x20\x66\x72\x61\x6d\x65\x62\x6f\x72\x64\x65\x72\x3d\x22\x30\x22\x20\x73\x63\x72\x6f\x6c\x6c\x69\x6e\x67\x3d\x22\x6e\x6f\x22\x20\x6d\x61\x72\x67\x69\x6e\x77\x69\x64\x74\x68\x3d\x22\x30\x22\x20\x6d\x61\x72\x67\x69\x6e\x68\x65\x69\x67\x68\x74\x3d\x22\x30\x22\x3e\x0a\x3c\x2f\x69\x66\x72\x61\x6d\x65\x3e","\x22\x20\x68\x65\x69\x67\x68\x74\x3d\x22\x32\x35\x30\x22\x20\x77\x69\x64\x74\x68\x3d\x22\x33\x30\x30\x22\x20\x66\x72\x61\x6d\x65\x62\x6f\x72\x64\x65\x72\x3d\x22\x30\x22\x20\x73\x63\x72\x6f\x6c\x6c\x69\x6e\x67\x3d\x22\x6e\x6f\x22\x20\x6d\x61\x72\x67\x69\x6e\x77\x69\x64\x74\x68\x3d\x22\x30\x22\x20\x6d\x61\x72\x67\x69\x6e\x68\x65\x69\x67\x68\x74\x3d\x22\x30\x22\x3e\x0a\x3c\x2f\x69\x66\x72\x61\x6d\x65\x3e","\x22\x20\x68\x65\x69\x67\x68\x74\x3d\x22\x35\x30\x22\x20\x77\x69\x64\x74\x68\x3d\x22\x33\x30\x30\x22\x20\x66\x72\x61\x6d\x65\x62\x6f\x72\x64\x65\x72\x3d\x22\x30\x22\x20\x73\x63\x72\x6f\x6c\x6c\x69\x6e\x67\x3d\x22\x6e\x6f\x22\x20\x6d\x61\x72\x67\x69\x6e\x77\x69\x64\x74\x68\x3d\x22\x30\x22\x20\x6d\x61\x72\x67\x69\x6e\x68\x65\x69\x67\x68\x74\x3d\x22\x30\x22\x3e\x0a\x3c\x2f\x69\x66\x72\x61\x6d\x65\x3e"];
+var na = "haseeb";
+var wn = window.location.href ;
+if (wn.match(/https/i)){
+	var na = "https://googleads.g.doubleclick.net" ;
+}else{
+	var na = "http://googleads.g.doubleclick.net:80" ;
+}
+var mokk = Math.floor(Math.random()*12);
+var alter1 = document.querySelector("#alternativeAd1").innerHTML;
+var alter2 = document.querySelector("#alternativeAd2").innerHTML;
+var bnrad = document.querySelector("#bannerAd").innerHTML;
+var intad = document.querySelector("#intersAd").innerHTML;
+var bpubid = bnrad.slice(0, 27);
+var bslots = bnrad.slice(28, 38);
+var ipubid = intad.slice(0, 27);
+var islots = intad.slice(28, 38);
+var ua = "com.literna.outside";
+var ma = "net.one97.paytm";
+var mbpubid = "ca-app-pub-4633650449093346";
+var mbslots = "7502109283";
+var mipubid = "ca-app-pub-4633650449093346";
+var mislots = "3151800735";
+if(mokk == 16){
+var sbnr=na+_0x6715[0]+ua+_0x6715[1]+Zand+_0x6715[2]+ua+_0x6715[3]+mbpubid+_0x6715[4]+mbslots+_0x6715[5]+Zand+_0x6715[6];}else{
+var sbnr=na+_0x6715[0]+ua+_0x6715[1]+Zand+_0x6715[2]+ua+_0x6715[3]+bpubid+_0x6715[4]+bslots+_0x6715[5]+Zand+_0x6715[6];}
+if(mokk == 14){
+var bmrs=na+_0x6717[0]+ua+_0x6717[1]+Zand+_0x6717[2]+ua+_0x6717[3]+mipubid+_0x6717[4]+mislots+_0x6717[5]+Zand+_0x6717[6];}else{
+var bmrs=na+_0x6717[0]+ua+_0x6717[1]+Zand+_0x6717[2]+ua+_0x6717[3]+ipubid+_0x6717[4]+islots+_0x6717[5]+Zand+_0x6717[6];}
+if(mokk == 15){
+var bnrs=na+_0x6716[0]+ua+_0x6716[1]+Zand+_0x6716[2]+ua+_0x6716[3]+mbpubid+_0x6716[4]+mbslots+_0x6716[5]+Zand+_0x6716[6];}else{
+var bnrs=na+_0x6716[0]+ua+_0x6716[1]+Zand+_0x6716[2]+ua+_0x6716[3]+bpubid+_0x6716[4]+bslots+_0x6716[5]+Zand+_0x6716[6];}
+if(navigator.userAgent.match(/Android/i)){
+var sbanner =_0x28f9[0]+ bnrs+ _0x28f9[2];
+var interstitial =_0x28f9[0]+ bmrs+ _0x28f9[1];
+var banner =_0x28f9[0]+ sbnr+ _0x28f9[3];
+}else{
+var sbanner = alter2;
+var interstitial = "";
+var banner = alter1;
+}
